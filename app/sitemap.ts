3d2262c3d4eed256880784cc76e20960a94d40d9
unknown <feodotevdima@gmail.com>
2026-03-01 17:03:54 +0300

    .

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -13,7 +13,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: `${baseUrl}/about`,
+      url: `${baseUrl}/catalog`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.8,
