abed835b971567bb7bf52e5714c21bdbcea1e919
unknown <feodotevdima@gmail.com>
2026-03-01 15:48:26 +0300

    .

---
+++ app/sitemap.ts
@@ -0,0 +1,22 @@
+import { MetadataRoute } from 'next'
+
+export const dynamic = 'force-static'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://tara-business.ru'
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.8,
+    },
+  ]
+}
